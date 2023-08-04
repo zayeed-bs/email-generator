f173f6a88a29de0c6849f48954d89b02cfe33180
Zayeed Bin Syed <74665895+zayeed-bs@users.noreply.github.com>
2023-08-04 14:33:16 -0600

    setup project

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+require('dotenv').config()
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+})
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
