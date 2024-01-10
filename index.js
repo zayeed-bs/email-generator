541cc56f18c11a9e4f216f34f6e646bac2192aec
zayeed-bs <zayeed.dev@gmail.com>
2024-01-09 19:07:04 -0600

    hotfix

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-let requestsCount = 0;
 
 app.use(express.static('public'));
 
@@ -18,8 +17,6 @@ app.get('/', (req, res) => {
 app.post('/generate-email', bodyParser.json(), async (req, res) => {  
   data = req.body;
 
-  requestsCount++;
-  console.log(`Request ${requestsCount}, ${req.socket.remoteAddress}`);
 
   response = await runPrompt(data.summary);
 
